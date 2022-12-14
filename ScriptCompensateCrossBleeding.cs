c8fb767132d478366f5eb38a88164729cadd48e3
Tiago Conceição <Tiago_caza@hotmail.com>
2022-12-14 16:53:31 +0000

    Remove comments

--- ScriptCompensateCrossBleeding.cs
+++ ScriptCompensateCrossBleeding.cs
@@ -40,67 +40,6 @@ public class ScriptCompensateCrossBleeding : ScriptGlobals
     {
         Progress.Reset("Changing layers", Operation.LayerRangeCount); // Sets the progress name and number of items to process
 
-        // Jan
-        /*
-        Layer[] newLayers = new Layer[SlicerFile.LayerCount];
-        Parallel.For(Operation.LayerIndexStart, Operation.LayerIndexEnd + 1,
-            CoreSettings.GetParallelOptions(Progress),
-            layerIndex =>
-            {
-                var layersBelowCount = layerIndex > LayerBleed.Value ? LayerBleed.Value : layerIndex;
-
-                using var layerMat = SlicerFile.Layers[layerIndex].LayerMat; // -> Don't use verbose .Layers, instead use: SlicerFile[layerIndex].LayerMat
-                var source = layerMat.GetDataByteSpan2D();
-
-                var targetMat = EmguExtensions.InitMat(new Size(layerMat.Width, layerMat.Height));
-                var target = targetMat.GetDataByteSpan2D(); // Never use Span2D when you don't require y,x, instead use Span
-
-                var occupancyMat = EmguExtensions.InitMat(new Size(layerMat.Width, layerMat.Height));
-                for (int i = 0; i != layersBelowCount; i++)
-                {
-                    using var mat = SlicerFile.Layers[layerIndex - i - 1].LayerMat;
-                    CvInvoke.Threshold(mat, mat, 1, 1, Emgu.CV.CvEnum.ThresholdType.Binary);
-                    CvInvoke.Add(mat, occupancyMat, occupancyMat);
-                    // var span = mat.GetDataByteSpan2D();
-
-                    // for (int y = 0; y != layerMat.Height; y++) {
-                    //     for (int x = 0; x != layerMat.Width; x++) {
-                    //         if (span[y, x] > 0) {
-                    //             occupancy[y, x] += 1;
-                    //         }
-                    //     }
-                    // }
-                }
-
-                var occupancy = occupancyMat.GetDataByteSpan2D();
-                for (int y = 0; y != layerMat.Height; y++)
-                {
-                    for (int x = 0; x != layerMat.Width; x++)
-                    {
-                        if (layersBelowCount == 0 || occupancy[y, x] == layersBelowCount)
-                            target[y, x] = source[y, x];
-                    }
-                }
-
-                // --> Will not be required, less operations and allocations
-                var newLayer = SlicerFile.Layers[layerIndex].Clone();
-                newLayer.LayerMat = targetMat;
-                newLayers[layerIndex] = newLayer;
-                Progress.LockAndIncrement();
-            });
-
-        // --> Will not be required, less operations and allocations
-        SlicerFile.SuppressRebuildPropertiesWork(() => {
-            Parallel.For(Operation.LayerIndexStart, Operation.LayerIndexEnd + 1,
-            CoreSettings.GetParallelOptions(Progress),
-            layerIndex =>
-            {
-                SlicerFile.Layers[layerIndex] = newLayers[layerIndex];
-            });
-        });
-        */
-
-        // sn4k3
         var originalLayers = SlicerFile.CloneLayers();
         Parallel.For(Operation.LayerIndexStart, Operation.LayerIndexEnd + 1,
             CoreSettings.GetParallelOptions(Progress),
