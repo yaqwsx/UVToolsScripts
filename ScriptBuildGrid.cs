f24d08b6df90175d17f1266beb4712f389c19eb5
Tiago Conceição <Tiago_caza@hotmail.com>
2022-12-18 08:40:38 +0000

    ScriptBuildGrid.cs: Code improvements (#1)

--- ScriptBuildGrid.cs
+++ ScriptBuildGrid.cs
@@ -1,19 +1,15 @@
-﻿using System;
+using System;
 using System.Drawing;
-using System.Threading.Tasks;
-using UVtools.Core;
 using UVtools.Core.Extensions;
 using UVtools.Core.Scripting;
-using UVtools.Core.Layers;
 using Emgu.CV;
-using Emgu.CV.Structure;
 using Emgu.CV.CvEnum;
 
 namespace UVtools.ScriptSample;
 
-public class ScriptPreventResinShrinkage : ScriptGlobals
+public class ScriptBuildGrid : ScriptGlobals
 {
-    readonly ScriptNumericalInput<int> GridSpacing = new()
+    readonly ScriptNumericalInput<ushort> GridSpacing = new()
     {
         Label = "Size of the initial grains",
         Unit = "px",
@@ -23,13 +19,24 @@ public class ScriptPreventResinShrinkage : ScriptGlobals
         Value = 200,
     };
 
-    readonly ScriptNumericalInput<int> GridWidth = new()
+    readonly ScriptNumericalInput<ushort> GridWidth = new()
     {
         Label = "Width of line",
         Unit = "px",
         Minimum = 1,
         Maximum = 500,
         Increment = 1,
+        Value = 1,
+    };
+
+    readonly ScriptNumericalInput<float> ExposureTime = new()
+    {
+        Label = "Exposure time",
+        Unit = "s",
+        Minimum = 0.1f,
+        Maximum = 1000f,
+        Increment = 0.5f,
+        DecimalPlates = 2,
         Value = 1,
     };
 
@@ -44,6 +51,9 @@ public class ScriptPreventResinShrinkage : ScriptGlobals
         Script.Version = new Version(0, 1);
         Script.UserInputs.Add(GridSpacing);
         Script.UserInputs.Add(GridWidth);
+
+        Script.UserInputs.Add(ExposureTime);
+        ExposureTime.Value = SlicerFile.BottomExposureTime;
     }
 
     /// <summary>
@@ -55,30 +65,30 @@ public class ScriptPreventResinShrinkage : ScriptGlobals
         return null;
     }
 
-    private Mat GenerateGridPattern() {
-        var pattern = EmguExtensions.InitMat(SlicerFile.Resolution);
-        var white = new MCvScalar(255, 255, 255);
+    private Mat GenerateGridPattern() 
+    {
+        var pattern = SlicerFile.CreateMat();
 
         for (int x = pattern.Size.Width / 2; x < pattern.Size.Width; x += GridSpacing.Value) {
             CvInvoke.Line(pattern,
                 new Point(x, 0),
                 new Point(x, pattern.Size.Height),
-                white, GridWidth.Value, LineType.FourConnected);
+                EmguExtensions.WhiteColor, GridWidth.Value, LineType.FourConnected);
             CvInvoke.Line(pattern,
                 new Point(pattern.Size.Width - x, 0),
                 new Point(pattern.Size.Width - x, pattern.Size.Height),
-                white, GridWidth.Value, LineType.FourConnected);
+                EmguExtensions.WhiteColor, GridWidth.Value, LineType.FourConnected);
         }
 
         for (int y = pattern.Size.Height / 2; y < pattern.Size.Height; y += GridSpacing.Value) {
             CvInvoke.Line(pattern,
                 new Point(0, y),
                 new Point(pattern.Size.Width, y),
-                white, GridWidth.Value, LineType.FourConnected);
+                EmguExtensions.WhiteColor, GridWidth.Value, LineType.FourConnected);
             CvInvoke.Line(pattern,
                 new Point(0, pattern.Size.Height - y),
                 new Point(pattern.Size.Width, pattern.Size.Height - y),
-                white, GridWidth.Value, LineType.FourConnected);
+                EmguExtensions.WhiteColor, GridWidth.Value, LineType.FourConnected);
         }
 
         return pattern;
@@ -90,20 +100,20 @@ public class ScriptPreventResinShrinkage : ScriptGlobals
     /// <returns>True if executes successfully to the end, otherwise false.</returns>
     public bool ScriptExecute()
     {
-        Progress.Reset("Changing layers", Operation.LayerRangeCount); // Sets the progress name and number of items to process
+        Progress.Reset("Changing layers", 2); // Sets the progress name and number of items to process
 
-        Layer[] newLayers = new Layer[2];
-        var pattern = GenerateGridPattern();
+        SlicerFile.Reallocate(2);
+        
+        using var pattern = GenerateGridPattern();
+        SlicerFile[0].LayerMat = pattern;
+        Progress++;
+        SlicerFile[0].CopyImageTo(SlicerFile[1]);
+        Progress++;
 
-        newLayers[0] = SlicerFile.Layers[0];
-        newLayers[0].LayerMat = pattern;
+        SlicerFile.BottomLayerCount = 2; // Sanitize bottom layer count
+        SlicerFile.BottomExposureTime = ExposureTime.Value;
+        SlicerFile.ExposureTime = ExposureTime.Value;
 
-        newLayers[1] = SlicerFile.Layers[1];
-        newLayers[1].LayerMat = pattern;
-
-        SlicerFile.SuppressRebuildPropertiesWork(() => {
-            SlicerFile.Layers = newLayers;
-        });
         return !Progress.Token.IsCancellationRequested;
     }
 }
