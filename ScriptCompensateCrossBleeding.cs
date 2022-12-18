d0c1260f8cb3e625d84b105e5c630c1068d5ee70
Tiago Conceição <Tiago_caza@hotmail.com>
2022-12-18 08:40:26 +0000

    ScriptCompensateCrossBleeding.cs: Code improvement (#2)

--- ScriptCompensateCrossBleeding.cs
+++ ScriptCompensateCrossBleeding.cs
@@ -1,22 +1,16 @@
-﻿using System;
+using System;
 using System.Drawing;
 using System.Threading.Tasks;
 using UVtools.Core;
 using UVtools.Core.Extensions;
 using UVtools.Core.Scripting;
-using UVtools.Core.Layers;
 using Emgu.CV;
-using Emgu.CV.Structure;
-using Emgu.CV.CvEnum;
-
-using Microsoft.Toolkit.HighPerformance;
-
 
 namespace UVtools.ScriptSample;
 
 public class ScriptCompensateCrossBleeding : ScriptGlobals
 {
-    readonly ScriptNumericalInput<int> LayerBleed = new()
+    readonly ScriptNumericalInput<ushort> LayerBleed = new()
     {
         Label = "Number of layers the exposure bleeds through",
         Unit = "layers",
@@ -37,64 +31,55 @@ public class ScriptCompensateCrossBleeding : ScriptGlobals
 
     public string? ScriptValidate()
     {
-        return null;
+        return SlicerFile.LayerCount < 2 
+            ? "This script requires at least 2 layers in order to run." 
+            : null;
     }
 
     public bool ScriptExecute()
     {
         Progress.Reset("Changing layers", Operation.LayerRangeCount); // Sets the progress name and number of items to process
 
-        Layer[] newLayers = new Layer[SlicerFile.LayerCount];
-        Parallel.For( Operation.LayerIndexStart, Operation.LayerIndexEnd + 1,
-            CoreSettings.GetParallelOptions(Progress),
-            layerIndex =>
-        {
-            var layersBelowCount = layerIndex > LayerBleed.Value ? LayerBleed.Value : layerIndex;
-
-            using var layerMat = SlicerFile.Layers[layerIndex].LayerMat;
-            var source = layerMat.GetDataByteSpan2D();
-
-            var targetMat = EmguExtensions.InitMat(new Size(layerMat.Width, layerMat.Height));
-            var target = targetMat.GetDataByteSpan2D();
-
-            var occupancyMat = EmguExtensions.InitMat(new Size(layerMat.Width, layerMat.Height));
-            for (int i = 0; i != layersBelowCount; i++) {
-                using var mat = SlicerFile.Layers[layerIndex - i - 1].LayerMat;
-                CvInvoke.Threshold(mat, mat, 1, 1, Emgu.CV.CvEnum.ThresholdType.Binary);
-                CvInvoke.Add(mat, occupancyMat, occupancyMat);
-                // var span = mat.GetDataByteSpan2D();
-
-                // for (int y = 0; y != layerMat.Height; y++) {
-                //     for (int x = 0; x != layerMat.Width; x++) {
-                //         if (span[y, x] > 0) {
-                //             occupancy[y, x] += 1;
-                //         }
-                //     }
-                // }
-            }
-
-            var occupancy = occupancyMat.GetDataByteSpan2D();
-            for (int y = 0; y != layerMat.Height; y++) {
-                for (int x = 0; x != layerMat.Width; x++) {
-                    if (layersBelowCount == 0 || occupancy[y, x] == layersBelowCount)
-                        target[y, x] = source[y, x];
-                }
-            }
-
-            var newLayer = SlicerFile.Layers[layerIndex].Clone();
-            newLayer.LayerMat = targetMat;
-            newLayers[layerIndex] = newLayer;
-            Progress.LockAndIncrement();
-        });
-
-        SlicerFile.SuppressRebuildPropertiesWork(() => {
-            Parallel.For( Operation.LayerIndexStart, Operation.LayerIndexEnd + 1,
+        var originalLayers = SlicerFile.CloneLayers();
+        Parallel.For(Operation.LayerIndexStart, Operation.LayerIndexEnd + 1,
             CoreSettings.GetParallelOptions(Progress),
             layerIndex =>
             {
-                SlicerFile.Layers[layerIndex] = newLayers[layerIndex];
+                var layersBelowCount = layerIndex > LayerBleed.Value ? LayerBleed.Value : layerIndex;
+
+                using var sourceMat = originalLayers[layerIndex].LayerMat;
+                var source = sourceMat.GetDataByteSpan();
+
+                using var targetMat = sourceMat.NewBlank();
+                var target = targetMat.GetDataByteSpan();
+
+                using var occupancyMat = sourceMat.NewBlank();
+                var occupancy = occupancyMat.GetDataByteSpan();
+
+                var sumRectangle = Rectangle.Empty;
+                for (int i = 0; i < layersBelowCount; i++)
+                {
+                    using var mat = originalLayers[layerIndex - i - 1].LayerMat;
+                    CvInvoke.Threshold(mat, mat, 1, 1, Emgu.CV.CvEnum.ThresholdType.Binary);
+                    CvInvoke.Add(mat, occupancyMat, occupancyMat);
+                    sumRectangle = sumRectangle.IsEmpty 
+                        ? originalLayers[layerIndex - i - 1].BoundingRectangle 
+                        : Rectangle.Union(sumRectangle, originalLayers[layerIndex - i - 1].BoundingRectangle);
+                }
+
+                // Spare a few useless cycles depending on model volume on LCD
+                var optimizedStatingPixelIndex = sourceMat.GetPixelPos(sumRectangle.Location);
+                var optimizedEndingPixelIndex = sourceMat.GetPixelPos(sumRectangle.Right, sumRectangle.Bottom);
+                for (var i = optimizedStatingPixelIndex; i < optimizedEndingPixelIndex; i++)
+                {
+                    if (layersBelowCount == 0 || occupancy[i] == layersBelowCount)
+                        target[i] = source[i];
+                }
+
+                SlicerFile[layerIndex].LayerMat = targetMat;
+                Progress.LockAndIncrement();
             });
-        });
+
         // return true if not cancelled by user
         return !Progress.Token.IsCancellationRequested;
     }
